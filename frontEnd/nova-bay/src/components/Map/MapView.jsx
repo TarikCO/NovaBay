4ac84d025ab9200f789f1010d6e6977ba1ee3273
Tarik Oliveira <tarik.castro.o@gmail.com>
2026-03-28 18:11:43 -0400

    MapLibre creation

--- frontEnd/nova-bay/src/components/Map/MapView.jsx
+++ frontEnd/nova-bay/src/components/Map/MapView.jsx
@@ -1,15 +1,49 @@
-import { useEffect } from 'react'
+import { useEffect, useState } from 'react'
 import maplibregl from 'maplibre-gl'
 import 'maplibre-gl/dist/maplibre-gl.css'
+import './MapView.css'
 
 function MapView() {
+  const [isLoading, setIsLoading] = useState(true)
+
   useEffect(() => {
+    const tampaCenter = [-82.4572, 27.9506]
+
     const map = new maplibregl.Map({
       container: 'map',
-      style: 'https://tiles.stadiamaps.com/styles/alidade_smooth.json',
-      center: [-82.4572, 27.9506],
+      style: 'https://tiles.stadiamaps.com/styles/alidade_smooth_dark.json',
+      center: tampaCenter,
       zoom: 11,
+      maxBounds: [[-83.5, 27.2], [-81.8, 28.5]],
+      minZoom: 9,
+      maxZoom: 18,
     })
+
+    map.addControl(new maplibregl.NavigationControl(), 'bottom-right')
+    map.addControl(
+      new maplibregl.GeolocateControl({
+        positionOptions: {
+          enableHighAccuracy: true,
+        },
+        trackUserLocation: true,
+      }),
+      'bottom-right',
+    )
+    map.addControl(new maplibregl.ScaleControl({ unit: 'imperial' }), 'bottom-left')
+
+    map.jumpTo({ center: tampaCenter, zoom: 8 })
+
+    const handleLoad = () => {
+      setIsLoading(false)
+      map.flyTo({
+        center: tampaCenter,
+        zoom: 11,
+        duration: 2000,
+        essential: true,
+      })
+    }
+
+    map.on('load', handleLoad)
 
     let activeMarker = null
 
@@ -21,15 +55,27 @@ function MapView() {
         activeMarker.remove()
       }
 
-      activeMarker = new maplibregl.Marker().setLngLat([lng, lat]).addTo(map)
+      const markerElement = document.createElement('div')
+      markerElement.className = 'map-marker'
+      markerElement.innerHTML = '<span class="map-marker-core"></span><span class="map-marker-pulse"></span>'
+
+      activeMarker = new maplibregl.Marker({ element: markerElement, anchor: 'center' })
+        .setLngLat([lng, lat])
+        .addTo(map)
     })
 
     return () => {
+      map.off('load', handleLoad)
       map.remove()
     }
   }, [])
 
-  return <div id="map" />
+  return (
+    <div className="map-view">
+      {isLoading && <div className="map-loading">Loading map...</div>}
+      <div id="map" />
+    </div>
+  )
 }
 
 export default MapView
